40d1d7553d072ea4265afaf46f60e726933514ec
vembu15 <154046651+vembu15@users.noreply.github.com>
2023-12-17 12:56:23 +0530

    Add files via upload

---
+++ TicketBooking/src/main/java/com/Theator.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Theator {
+//private int no_of_screen=12;
+//private double thicket_price=120;
+//public String s="1.Radhakrishna\n2.Charli\n3.Teaddy";
+
+public static void main(String[] args) {
+	int no_of_screen=12;
+	 double thicket_price=120;
+	 String s="1.Radhakrishna\n2.Charli\n3.Teaddy";
+	 System.out.println(s);
+
+}
+
+}
